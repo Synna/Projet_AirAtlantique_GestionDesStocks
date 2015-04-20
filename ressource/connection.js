d090bcf3d243ae60098a263149c95b281a230b79
Antoine Garrec <antoine.garrec@epsi.fr>
2015-04-20 13:18:58 +0200

    V2.0
    Update

--- ressource/connection.js
+++ ressource/connection.js
@@ -1,19 +1,139 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'stocks'
-});
+var mysql = require('mysql');
+var connection = mysql.createConnection(
+    {
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'stocks',
+    }
+);
 
-var selectuser = function (login, password){
-	connection.connect();
-  var queryString = 'SELECT id, login, password, access from user WHERE login = "'+ login + '"';    //je prépare ma requête
-	connection.query(queryString, function(err, rows, fields) {                                       //j’exécute ma requête
-  connection.end();
-    console.log(rows[0]);
-    callback(rows[0]);                          //je veux retourner la réponse avec tout les valeur pour faire  
-                                                //un traitement et des redirection en fonction de sont accès 
+var selectuser = function (login, password, callback){
+  var queryString = 'SELECT id, login, password, access from user WHERE login = "'+ login + '"';
+	connection.query(queryString, function(err, rows, fields) {
+    callback(rows[0]);
 });
 };
 exports.selectuser = selectuser;
+
+var lsproduit = function (avion, callback){
+  var queryString = 'SELECT qte.qte, produits.nom from qte, produits WHERE qte.idproduits = produits.idproduits AND qte.idavion = "'+ avion + '"';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.lsproduit = lsproduit;
+
+var produits = function (callback){
+  var queryString = 'SELECT idproduits, nom, prix from produits';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.produits = produits;
+
+var lsavions = function (callback){
+  var queryString = 'SELECT id, avion.idaeroport, type, immatriculation, nom from avion, aeroport WHERE avion.idaeroport = aeroport.idaeroport ';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.lsavions = lsavions;
+
+var lsproduitae = function (aeroport, callback){
+  var queryString = 'SELECT qteaeroport.qte, produits.nom from qteaeroport, produits WHERE qteaeroport.idproduits = produits.idproduits AND qteaeroport.idaeroport = "'+ aeroport + '"';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.lsproduitae = lsproduitae;
+
+var lsaeroports = function (callback){
+  var queryString = 'SELECT idaeroport, nom, pays, ville from aeroport';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.lsaeroports = lsaeroports;
+
+var infoaeroport = function (aeroport, callback){
+  var queryString = 'SELECT idaeroport, nom, pays, ville from aeroport WHERE idaeroport = "'+ aeroport + '"';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.infoaeroport = infoaeroport;
+
+var afficheruser = function (callback){
+  var queryString = 'SELECT id, login from user';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.afficheruser = afficheruser;
+
+var ajoutuser = function (loginajouter, passwordajouter, access, callback){
+  var queryString = 'INSERT INTO user (login, password, access) VALUES ("'+ loginajouter +'", "'+ passwordajouter +'", "'+ access +'");';
+	connection.query(queryString, function(err, fields) {
+    if (err) {
+    var msg = 'il y a une erreur';
+    callback(msg);
+    }
+    else {
+    var msg = 'Done';
+    callback(msg);
+    }
+});
+};
+exports.ajoutuser = ajoutuser;
+
+
+
+var insertqte = function (nombre, idduproduit, idaeroport, callback){
+    var queryString = 'INSERT INTO qteaeroport (idaeroport, idproduits, qte) VALUES ("'+ idaeroport + '", "'+ idduproduit + '", "'+ nombre + '");';
+    connection.query(queryString, function(err, fields) {});
+};
+exports.insertqte = insertqte;
+
+var ajouterproduit = function (nomproduit, prixproduit, callback){
+    var queryString = 'INSERT INTO produits (nom, prix) VALUES ("'+ nomproduit + '", "'+ prixproduit + '");';
+    connection.query(queryString, function(err, fields) {});
+};
+exports.ajouterproduit = ajouterproduit;
+
+var ajouteravion = function (idaeroport, typeavion, immatriculation, callback){
+    var queryString = 'INSERT INTO avion (idaeroport, type, immatriculation) VALUES ("'+ idaeroport + '", "'+ typeavion + '", "'+ immatriculation + '");';
+    connection.query(queryString, function(err, fields) {});
+};
+exports.ajouteravion = ajouteravion;
+
+var ajouteraeroport = function (nom, pays, ville, callback){
+    var queryString = 'INSERT INTO aeroport (nom, pays, ville) VALUES ("'+ nom + '", "'+ pays + '", "'+ ville + '");';
+    connection.query(queryString, function(err, fields) {});
+};
+exports.ajouteraeroport = ajouteraeroport;
+
+var ajoutqte = function (nombre, idduproduit, idaeroport, callback){
+  var queryString = 'SELECT qte from qteaeroport WHERE idaeroport = "'+ idaeroport + '" AND idproduits = "'+ idduproduit + '"';
+  connection.query(queryString, function(err, rows, fields) {
+    var temp = parseFloat(rows[0].qte) + parseFloat(nombre);
+    var queryString = 'UPDATE qteaeroport SET qte = "'+ temp + '" WHERE idaeroport = "'+ idaeroport + '" AND idproduits = "'+ idduproduit + '";';
+    connection.query(queryString, function(err, fields) {});
+  });
+};
+exports.ajoutqte = ajoutqte;
+
+var deletuser = function (useradelet, callback){
+  var queryString = 'DELETE FROM user WHERE id='+ useradelet + '';
+  connection.query(queryString, function(err, fields) {
+  if (err) {
+    var msg = 'il y a une erreur';
+    callback(msg);
+  }
+  else {
+    var msg = 'Done';
+    callback(msg);
+  }
+});
+};
+exports.deletuser = deletuser;
--- serveur.js
+++ serveur.js
@@ -2,25 +2,173 @@ var reqmysql = require('./ressource/connection');
 var express = require('express');
 var session = require('cookie-session');
 var bodyParser = require('body-parser');
+var http = require('http');
+var fs = require('fs');
 var app = express();
+var server = http.createServer(app);
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-
+var sess;
+var io = require('socket.io').listen(server);
+var lsavions;
+var lsproduit = '';
+var lsaeroports;
 app.use(session({secret: 'todotopsecret'}));
 app.use(bodyParser.urlencoded({
-  extended: true
+extended: true
 }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) { 
-    res.render('index.ejs');
+
+
+
+app.get('/', function(req, res) {
+var msg ='';
+res.render('index.ejs', {msg: msg});
 });
 
 app.post('/connexion', function(req, res) {
-	var login = req.body.login;
-	var password = req.body.password;
-	reqmysql.selectuser(login, password);
-})
+  var login = req.body.login;
+  var password = req.body.password;
+  reqmysql.selectuser(login, password, function callback (result){
+    try{
+  if (result.login == login && result.password == password) {
+    sess = req.session;
+    sess.access = result.access;
+    res.render('admin.ejs', {access: sess.access});
+    };
+    if (result.login == login && result.password != password) {
+    var msg = 'Mot de passe incorrect';
+    res.render('index.ejs', {msg: msg});
+    };
+    }
+    catch(msg){
+    var msg = 'Le login existe pas.';
+      res.render('index.ejs', {msg: msg});
+    }
+  });
+});
 
-.use(express.static(__dirname + '/css'))
-.listen(8080);
+app.get('/admin', function(req, res) {
+  var msg ='';
+res.render('admin.ejs', {msg: msg});
+});
+
+app.get('/ajouter', function(req, res) {
+res.render('ajouter.ejs');
+});
+
+app.get('/adminapplication', function(req, res) {
+  var user;
+reqmysql.afficheruser(function callback (result){
+  user = result;
+res.render('adminappli.ejs', {msg: user});
+});
+});
+
+app.get('/aeroports', function(req, res) {
+reqmysql.lsaeroports(function callback (result){
+lsaeroports = result;
+res.render('lsaeroport.ejs', {msg: lsaeroports});
+});
+});
+
+app.post('/aeroportsdetail', function(req, res) {
+var aeroport = req.body.aeroport;
+reqmysql.lsaeroports(function callback (result){
+lsaeroports = result;
+reqmysql.lsproduitae(aeroport, function callback (result2){
+lsproduit = result2;
+reqmysql.infoaeroport(aeroport, function callback (result3){
+var infoaeroport = result3;
+res.render('lsaeroportdetail.ejs', {msg: lsaeroports, lsproduit: lsproduit, infoaeroport: infoaeroport});
+});
+});
+});
+});
+
+app.post('/aeroportsgere', function(req, res) {
+var aeroport = req.body.aeroport;
+reqmysql.lsproduitae(aeroport, function callback (result1){
+lsproduit = result1;
+reqmysql.infoaeroport(aeroport, function callback (result2){
+var infoaeroport = result2;
+reqmysql.produits(function callback (result3){
+var produits = result3;
+res.render('lsaeroportgere.ejs', {lsproduit: lsproduit, infoaeroport: infoaeroport, produits: produits});
+});
+});
+});
+});
+
+app.post('/ajouterproduit', function(req, res) {
+  var nomproduit = req.body.nomproduit;
+  var prixproduit = req.body.prixproduit;
+  reqmysql.ajouterproduit(nomproduit, prixproduit, function callback (result){});
+res.redirect('/ajouter');
+});
+
+app.post('/ajouteravion', function(req, res) {
+  var idaeroport = req.body.idaeroport;
+  var typeavion = req.body.typeavion;
+  var immatriculation = req.body.immatriculation;
+  reqmysql.ajouteravion(idaeroport, typeavion, immatriculation, function callback (result){});
+res.redirect('/ajouter');
+});
+
+app.post('/ajouteraeroport', function(req, res) {
+  var nom = req.body.nom;
+  var pays = req.body.pays;
+  var ville = req.body.ville;
+  reqmysql.ajouteraeroport(nom, pays, ville, function callback (result){});
+res.redirect('/ajouter');
+});
+
+app.post('/ajouterqte', function(req, res) {
+  var nombre = req.body.nombre;
+  var idduproduit = req.body.idduproduit;
+  var idaeroport = req.body.idaeroport;
+  var actions = req.body.actions
+  if (actions == "Ajouter") {
+  reqmysql.ajoutqte(nombre, idduproduit, idaeroport, function callback (result){});
+  };
+  if (actions == "Insert") {
+  reqmysql.insertqte(nombre, idduproduit, idaeroport, function callback (result){});
+  };
+res.redirect('/aeroports');
+});
+
+app.get('/lsavions', function(req, res) {
+reqmysql.lsavions(function callback (result){
+lsavions = result;
+res.render('lsavion.ejs', {msg: lsavions});
+});
+});
+
+app.post('/aviondetail', function(req, res) {
+var avion = req.body.avion;
+reqmysql.lsavions(function callback (result){
+lsavions = result;
+reqmysql.lsproduit(avion, function callback (result2){
+lsproduit = result2;
+res.render('lsaviondetail.ejs', {msg: lsavions, lsproduit: lsproduit});
+});
+});
+});
+
+app.post('/deletuser', function(req, res) {
+  var useradelet = req.body.useradelet;
+  reqmysql.deletuser(useradelet, function callback (result){
+  });
+res.redirect('/adminapplication');
+});
+
+app.post('/ajouteruser', function(req, res) {
+  var loginajouter = req.body.loginajouter;
+  var passwordajouter = req.body.passwordajouter;
+  var access = req.body.access;
+  reqmysql.ajoutuser(loginajouter, passwordajouter, access, function callback (result){
+  });
+res.redirect('/adminapplication');
+})
+.use(express.static(__dirname + '/css'));
+server.listen(8080);
